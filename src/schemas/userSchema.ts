0d532e35794cdcf6df0894cd619fac23b3bb0b02
Piotr Morawski <piotrmorawski90@gmail.com>
2019-11-25 19:17:54 +0100

    add first grapql schema

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-server';
+
+export default gql`
+  type User {
+    id: ID!
+    email: String!
+  }
+
+  type Token {
+    token: String!
+  }
+
+  extend type Query {
+    user(id: ID!): User!
+    login(name: String!, password: String!): Token!
+  }
+
+  extend type Mutation {
+    createUser(name: String!, password: String!): User!
+  }
+`;
